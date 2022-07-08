ef1b14a115754b2d4c5a57f855c160c5960331b7
Clement-Kwa <20002694@myrp.edu.sg>
2022-07-08 11:15:24 +0800

    initial commit

--- web/test.php
+++ web/test.php
@@ -3,13 +3,13 @@
 <html lang="en">
 <head>
         <!-- fix: No viewport attribute with specified width and scale -->
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- fix: No Meta description attribute with specified -->
-<meta name="description" content="Hello World">
+<meta name="description" content="Author: A.N. Author,
+    Illustrator: P. Picture, Category: Books, Price: $17.99,
+    Length: 784 pages">
     <!-- fix: No Title tag specified -->
-<title>
-Test PHP
-</title>
+<title>Test PHP page website for c339</title>
 </head>
 <body>
   Hello World!
