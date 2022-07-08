c7b5a0c07959a38ee6df79958852e71342b986e3
Clement-Kwa <20002694@myrp.edu.sg>
2022-07-08 11:23:23 +0800

    initial commit

--- web/test.php
+++ web/test.php
@@ -2,14 +2,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-        <!-- fix: No viewport attribute with specified width and scale -->
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- fix: No viewport attribute with specified width and scale -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- fix: No Meta description attribute with specified -->
-<meta name="description" content="Aewfewf efwfw">
+    <meta name="description" content="Aewfewf efwfw">
     <!-- fix: No Title tag specified -->
-<title>Test PHP page website for c339</title>
+    <title>Test PHP page website for c339</title>
 </head>
 <body>
-  Hello World!
+    Hello World!
 </body>
+
 </html>
