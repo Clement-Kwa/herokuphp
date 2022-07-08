1ee9ac0b38df6da61307509758a0af60f95ee79d
Clement-Kwa <20002694@myrp.edu.sg>
2022-07-08 11:00:34 +0800

    initial commit

--- web/test.php
+++ web/test.php
@@ -1,4 +1,11 @@
-<html>
+<!-- fix: No lang attribute specified -->
+<html lang="en">
+    <meta name="viewport">
+<!-- fix: No viewport size specified -->
+<title>
+
+</title>
+<!-- fix: No Title tag specified -->
 <head>
 </head>
 <body>
