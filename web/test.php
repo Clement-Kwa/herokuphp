3dd01c04312017c0c22d71119ab5bb73fa151406
Clement-Kwa <20002694@myrp.edu.sg>
2022-07-08 11:20:07 +0800

    initial commit

--- web/test.php
+++ web/test.php
@@ -5,9 +5,7 @@
         <!-- fix: No viewport attribute with specified width and scale -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- fix: No Meta description attribute with specified -->
-<meta name="description" content="Author: A.N. Author,
-    Illustrator: P. Picture, Category: Books, Price: $17.99,
-    Length: 784 pages">
+<meta name="description" content="Aewfewf efwfw">
     <!-- fix: No Title tag specified -->
 <title>Test PHP page website for c339</title>
 </head>
